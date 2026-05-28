68ef69f4229adf53ace19cb307c197f1acf0eb00
rajkumar-pitroda_totalsyn <rajkumar.pitroda@totalsynergy.com>
2026-05-28 17:07:37 +0530

    Added Base service

---
+++ src/modules/base/base.service.ts
@@ -0,0 +1,30 @@
+import { DeepPartial, ObjectLiteral } from "typeorm";
+import { BaseRepository } from "./base.repository.ts";
+
+export class BaseService<T extends ObjectLiteral> {
+  constructor(private repo: BaseRepository<T>) {}
+
+  create = async (data: DeepPartial<T>): Promise<T> => {
+    return await this.repo.create(data);
+  };
+
+  getAll = async (): Promise<T[]> => {
+    return await this.repo.getAll();
+  };
+
+  getById = async (id: T["id"]): Promise<T | null> => {
+    return await this.repo.getById(id);
+  };
+
+  getByIdOrThrow = async (id: T["id"]): Promise<T> => {
+    return await this.repo.getByIdOrThrow(id);
+  };
+
+  update = async (id: T["id"], data: DeepPartial<T>): Promise<T["id"]> => {
+    return await this.repo.update(id, data);
+  };
+
+  delete = async (id: T["id"]): Promise<T> => {
+    return await this.repo.delete(id);
+  };
+}
