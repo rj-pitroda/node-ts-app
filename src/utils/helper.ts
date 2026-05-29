0b2bca82dd05a55b26422871e28f34d12e0b0756
raj.pitroda <raj.p@york.ie>
2026-05-29 10:59:38 +0530

    Added new fields for env extraction

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -4,4 +4,6 @@ export const ENV_VAR = {
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_PASSWORD: process.env.DB_PASSWORD,
+  NODE_ENV: process.env.NODE_ENV,
+  IS_DEV_ENV: process.env.NODE_ENV === "development",
 };
