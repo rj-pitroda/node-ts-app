0a5f9d9791c8db4db6b9f133a05c442f95ab2973
raj.pitroda <raj.p@york.ie>
2026-06-04 13:05:08 +0530

    Added 404 not found route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import userRouter from "../modules/user/user.route.ts";
+import { sendErrorResponse } from "../shared/sendResponse.ts";
+import { ERROR_MESSAGES } from "../shared/constants/error.ts";
 
 const appRouter = Router();
 
 // user router
 appRouter.use("/user", userRouter);
 
+// 404 route
 appRouter.use("/", (req, res) => {
-  return res.send("Welcome to Nodejs project");
+  return sendErrorResponse(res, {
+    message: ERROR_MESSAGES.API_ENDPOINT_NOT_FOUND,
+    statusCode: 404,
+  });
 });
 
 export default appRouter;
